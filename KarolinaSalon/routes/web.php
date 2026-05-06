146cd26dc1492368c7d408d170d9dfdbc60e1b24
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-05-05 18:06:01 -0600

    Controllers
    Controllers

--- KarolinaSalon/routes/web.php
+++ KarolinaSalon/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/{create}', function () {
+    return "Pagina de creación de usuarios";
+});
+
+
+Route::get('users/{nombre}/{nacionalidad}', function ($nombre, $nacionalidad) {
+    return "Bienvenido usuario: $nombre, De la Nacionalidad: $nacionalidad";
+});
