bac873807feb23c558a4c7fe390153c6aafd4e21
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-05-07 14:02:59 -0600

    arreglo

--- KarolinaSalon/app/Http/Controllers/UsuarioController.php
+++ KarolinaSalon/app/Http/Controllers/UsuarioController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+   public function index()
+   {
+       return "Bienvenido a la página de usuarioController";
+   }
 }
--- KarolinaSalon/routes/web.php
+++ KarolinaSalon/routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ✅ Solo una ruta '/'
+Route::get('/', [UsuarioController::class, 'index']);
 
 Route::get('users/{create}', function () {
     return "Pagina de creación de usuarios";
 });
-
 
 Route::get('users/{nombre}/{nacionalidad}', function ($nombre, $nacionalidad) {
     return "Bienvenido usuario: $nombre, De la Nacionalidad: $nacionalidad";
